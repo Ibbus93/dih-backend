927b43a64aaccb0a0767e4879e549fa891cdc87e
Ibbus93 <ibbus93@gmail.com>
2019-06-07 18:17:57 +0200

    Digital Finance backend - easy version

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ const app = express();
 /** Export of app, passport and the right configuration **/
 module.exports = { app };
 
-/** Configuration of express, routes and passport **/
+/** Configuration of express and routes **/
 require('./config/express')(app);
 require('./config/routes')(app);
